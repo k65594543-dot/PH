77f74967c96c4b3f6c6f49a54970954c28fc42cc
k65594543-dot <k65594543@gmail.com>
2025-09-30 10:21:13 +0300

    Create Sw.js

---
+++ Sw.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('v1').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/icon-192.png',
+        '/icon-512.png'
+        // Add any other assets like CSS, JS, images if you have them
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
